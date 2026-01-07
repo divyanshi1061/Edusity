cf2dbe58f0d8c4885dea85527a6966bff5360e76
dictalk <divyanshikhandelwal755@gmail.com>
2026-01-07 23:16:58 +0530

    commit

--- edusity/src/Components/Navbar/Navbar.jsx
+++ edusity/src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,8 @@ import './Navbar.css'
 import logo from '../../assets/logo.png'
 import { useState } from 'react'    
 import { Link } from 'react-scroll';
+import menu_icon from '../../assets/menu-icon.png'
+
 const Navbar = () => {
 
 const[sticky, setSticky] = useState(false);
@@ -15,11 +17,14 @@ useEffect(() => {
         }
     })
 }, []);
+const[mobileMenu, setMobileMenu] = useState(false);
+const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);}
 
   return (
     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
         <img src={logo} alt="Logo" className='logo' />
-        <ul>
+        <ul className={mobileMenu?'':'hide-mobile-menu'}>
             <li><Link to="hero" smooth={true} offset={0} duration={500}>Home</Link></li>
             <li><Link to="programs" smooth={true} offset={-260} duration={500}>Program</Link></li>
             <li><Link to="about" smooth={true} offset={-150} duration={500}>About Us</Link></li>
@@ -27,6 +32,7 @@ useEffect(() => {
             <li><Link to="testimonials" smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
             <li><Link to="contact" smooth={true} offset={-260} duration={500}><button className='btn'>Contact us</button></Link></li>
         </ul>
+        <img src={menu_icon} alt=""  className='menu-icon' onClick={toggleMenu}/>
     </nav>
   )
 }
